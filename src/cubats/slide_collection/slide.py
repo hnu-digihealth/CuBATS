feat: upgraded calculate_percentage_and_score so it includes correct weighting as well as score assignment if one zone >66.6 % of pixels in the tile. Adapted tests to changes as well as summarize_quantification_results

src/cubats/slide_collection/slide.py
@@ -16,7 +16,6 @@ from tqdm import tqdm
 
 # CuBATS
 import cubats.logging_config as log_config
-from cubats import cutils as cutils
 from cubats.slide_collection import tile_processing
 
 
@@ -264,6 +263,14 @@ class Slide(object):
             0,
         )
 
+        # Score counts
+        score_counts = {
+            "High Positive": 0,
+            "Positive": 0,
+            "Low Positive": 0,
+            "Negative": 0,
+            "Background": 0
+        }
         # Iterate through tiles_dict and sum up number of pixels in each zone as well as the total number of pixels
         for i in self.detailed_quantification_results:
             if self.detailed_quantification_results[i]["Flag"] == 1:
@@ -273,14 +280,31 @@ class Slide(object):
                 sum_z4 += self.detailed_quantification_results[i]["Zones"][3]
                 background += self.detailed_quantification_results[i]["Zones"][4]
                 count += self.detailed_quantification_results[i]["Px_count"]
+                score_name = self.detailed_quantification_results[i]["Score"]
+                score_counts[score_name] += 1
 
         # Calculate percentages and scores
         zones = [sum_z1, sum_z2, sum_z3, sum_z4, background]
         percentages = [0] * 5
-        scores = [0] * 5
         for i in range(len(zones)):
             percentages[i] = (zones[i] / count) * 100
-            scores[i] = (zones[i] * (len(zones) - i)) / count
+
+        # Calculate the summary score for the slide using weights
+        weights = {
+            "High Positive": 4,
+            "Positive": 3,
+            "Low Positive": 2,
+            "Negative": 1,
+            "Background": 0
+        }
+
+        total_processed_tiles = sum(score_counts.values())
+        weighted_scores = [score_counts[score] * weights[score]
+                           / total_processed_tiles for score in score_counts]
+        max_score_index = np.argmax(weighted_scores[:4])  # Exclude Background
+        zone_names = ["High Positive", "Positive",
+                      "Low Positive", "Negative", "Background"]
+        slide_score = zone_names[max_score_index]
 
         # Create dictionary and append to quantification_results
         self.quantification_summary["Name"] = self.name
@@ -292,8 +316,7 @@ class Slide(object):
         self.quantification_summary["Negative (%)"] = round(percentages[3], 2)
         self.quantification_summary["Background (%)"] = round(
             percentages[4], 2)
-        self.quantification_summary["Score"] = cutils.get_score_name(
-            scores)  # TODO necessary?
+        self.quantification_summary["Score"] = slide_score
         end_time_summarize = time()
         self.logger.debug(
             f"Finished summarizing quantification results for slide: {self.name} in \

src/cubats/slide_collection/tile_processing.py
@@ -10,9 +10,9 @@ Credits:
   2001. PMID: 11531144. Source: https://scikit-image.org/docs/stable/auto_examples/color_exposure/ \
     plot_ihc_color_separation.html
 
-- The `calculate_pixel_intensity` function is inspired by the work of Varghese et al. (2014) "IHC Profiler: An Open
-  Source Plugin for the Quantitative Evaluation and Automated Scoring of Immunohistochemistry Images of Human Tissue
-  Samples."
+- The `calculate_pixel_intensity` and calculate_percentage_and_score function are inspired by the work of Varghese et
+al. (2014) "IHC Profiler: An Open Source Plugin for the Quantitative Evaluation and Automated Scoring of
+Immunohistochemistry Images of Human Tissue Samples."
 
 Last Modified: 2023-10-05
 """
@@ -146,7 +146,6 @@ def ihc_stain_separation(
             - ihc_d (Image): DAB (3',3'-Diaminobenzidine) stain of the image.
             - ihc_h (Image): Hematoxylin stain of the image if hematoxylin=True, otherwise None.
             - ihc_e (Image): Eosin stain of the image if eosin=True, otherwise None.
-
     """
     # convert RGB image to HED using prebuild skimage method
     ihc_hed = rgb2hed(ihc_rgb)
@@ -171,11 +170,11 @@ def calculate_pixel_intensity(image):
     between 0-255. Intensities above 235 are predominantly background or fatty tissues and do not contribute to
     pathological scoring:
 
-        - Zone 1 = High positive (intensity: 0-60)
-        - Zone 2 = Positive (intensity: 61-120)
-        - Zone 3 = Low positive (intensity: 121-180)
-        - Zone 4 = negative (intensity: 181-235)
-        - Zone 5: White space or fatty tissue (intensity: 236-255)
+        - Zone 1: High positive (intensity: 0-60)
+        - Zone 2: Positive (intensity: 61-120)
+        - Zone 3: Low positive (intensity: 121-180)
+        - Zone 4: Negative (intensity: 181-235)
+        - Zone 5: Background or fatty tissues (intensity: 236-255)
 
     After calculating pixel intensities this function calculates percentage contribution of each of the zones as well
     as the a pathology score.
@@ -209,8 +208,7 @@ def calculate_pixel_intensity(image):
     zones = np.zeros(5)
     # pixelcount
     pixelcount = 0
-    # iterates through each pixel in the image and assigns it to one of the
-    # intensity zones
+    # Assigns each pixel to an intensity zone
     for y in range(h):
         for x in range(w):
             if gray_scale_ubyte[x, y] < 61:  # High positive tissue
@@ -230,20 +228,28 @@ def calculate_pixel_intensity(image):
                 pixelcount += 1
                 img_analysis[x, y] = gray_scale_ubyte[x, y]
             else:
-                # White space aka. fatty tissue needed for calculation in
-                # respect to actual tissue
+                # Background or fatty tissue needed for calculation with respect to actual tissue
                 zones[4] += 1
                 pixelcount += 1
 
-    percentage, score = calculate_score(zones, pixelcount)
+    percentage, score = calculate_percentage_and_score(zones, pixelcount)
 
     return hist, hist_centers, zones, percentage, score, pixelcount, img_analysis
 
 
-def calculate_score(zones, count):
+def calculate_percentage_and_score(zones, pixel_count):
     """
     Calculates the percentage of pixels in each zone relative to the total pixel count and computes a score for each
-    zone. TODO Check for Score and >66,6% px intensity
+    zone. If more than 66.6% of the total pixels are attributed to a single zone, that zone's score is assigned. Else,
+    the score for each zone is calculated using this formula:
+
+    .. math::
+
+        \\text{Score} = \\frac{(\\text{number of pixels in zone} \\times \\text{weight of zone})}{\\text{total
+        pixels in image}}
+
+    with weights 4 for the high positive zone, 3 for the positive zone, 2 for the low positive zone, 1 for the negative
+    zone, and 0 for the background. The final score is the maximum score among all zones.
 
     Args:
         zones (ndarray): Array containing amount of pixels from each zone
@@ -252,22 +258,36 @@ def calculate_score(zones, count):
     Returns:
         tuple: A tuple containing:
             - percentage (ndarray): Array containing the percentage of pixels in each zone.
-            - score (ndarray): Array containing the calculated score for each zone.
+            - score (str): Name of the zone if it exceeds 66.6%, otherwise the name of the zone with the highest score.
 
     Raises:
         ZeroDivisionError: If pixel count is zero.
-
-
+        ValueError: If all zones have zero pixels.
     """
-    if count == 0:
+    if pixel_count == 0:
         raise ZeroDivisionError("Count cannot be zero")
-    percentage = np.zeros(zones.size)
-    score = np.zeros(zones.size)
-    for i in range(zones.size):
-        percentage[i] = (zones[i] / count) * 100
-        score[i] = (zones[i] * (zones.size - (i + 1))) / count
 
-    return percentage, score
+    if np.sum(zones) == 0:
+        raise ValueError("All zones have zero pixels")
+
+    percentage = np.zeros(zones.size)
+    zone_names = ["High Positive", "Positive",
+                  "Low Positive", "Negative", "Background"]
+    weights = [4, 3, 2, 1, 0]  # Weights for each zone
+
+    for i in range(zones.size):
+        percentage[i] = (zones[i] / pixel_count) * 100
+
+    # Check if any zone exceeds 66.6%
+    for i in range(zones.size):
+        if percentage[i] > 66.6:
+            return percentage, zone_names[i]
+
+    # Else calculate the score for each zone and determine highest score
+    scores = [(zones[i] * weights[i]) / pixel_count for i in range(zones.size)]
+    max_score_index = np.argmax(scores[:4])
+
+    return percentage, zone_names[max_score_index]
 
 
 def mask_tile(tile, mask):

tests/tile_processing_test.py
@@ -7,10 +7,9 @@ import numpy as np
 from PIL import Image
 
 # CuBATS
-from cubats.slide_collection.tile_processing import (calculate_pixel_intensity,
-                                                     calculate_score,
-                                                     ihc_stain_separation,
-                                                     quantify_tile)
+from cubats.slide_collection.tile_processing import (
+    calculate_percentage_and_score, calculate_pixel_intensity,
+    ihc_stain_separation, quantify_tile)
 
 
 class TestQuantifyTile(unittest.TestCase):
@@ -356,45 +355,107 @@ class TestCalculatePixelIntensity(unittest.TestCase):
 
 class TestCalculateScore(unittest.TestCase):
 
-    def test_normal_case(self):
-        zones = np.array([10, 20, 30, 40, 0])
-        count = 100
-        expected_percentage = np.array([10.0, 20.0, 30.0, 40.0, 0.0])
-        expected_score = np.array([0.4, 0.6, 0.6, 0.4, 0.0])
-
-        percentage, score = calculate_score(zones, count)
-
-        np.testing.assert_array_almost_equal(percentage, expected_percentage)
-        np.testing.assert_array_almost_equal(score, expected_score)
-
     def test_edge_case_zeros(self):
-        zones = np.array([0, 0, 0, 0])
+        zones = np.array([0, 0, 0, 0, 0])
         count = 100
-        expected_percentage = np.array([0.0, 0.0, 0.0, 0.0])
-        expected_score = np.array([0.0, 0.0, 0.0, 0.0])
 
-        percentage, score = calculate_score(zones, count)
-
-        np.testing.assert_array_almost_equal(percentage, expected_percentage)
-        np.testing.assert_array_almost_equal(score, expected_score)
+        with self.assertRaises(ValueError):
+            calculate_percentage_and_score(zones, count)
 
     def test_invalid_input_zero_count(self):
-        zones = np.array([10, 20, 30, 40])
+        zones = np.array([10, 20, 30, 40, 0])
         count = 0
 
         with self.assertRaises(ZeroDivisionError):
-            calculate_score(zones, count)
+            calculate_percentage_and_score(zones, count)
 
     def test_large_numbers(self):
         zones = np.array([1000000, 2000000, 3000000, 4000000, 0])
         count = 10000000
         expected_percentage = np.array([10.0, 20.0, 30.0, 40.0, 0.0])
-        expected_score = np.array([0.4, 0.6, 0.6, 0.4, 0.0])
+        expected_score = "Positive"  # Based on the weights and the highest score
 
-        percentage, score = calculate_score(zones, count)
+        percentage, score = calculate_percentage_and_score(zones, count)
 
         np.testing.assert_array_almost_equal(percentage, expected_percentage)
-        np.testing.assert_array_almost_equal(score, expected_score)
+        self.assertEqual(score, expected_score)
+
+    def test_dominant_zone(self):
+        zones = np.array([70, 10, 10, 5, 5])
+        count = 100
+        expected_percentage = np.array([70.0, 10.0, 10.0, 5.0, 5.0])
+        expected_score = "High Positive"  # More than 66.6% in the first zone
+
+        percentage, score = calculate_percentage_and_score(zones, count)
+
+        np.testing.assert_array_almost_equal(percentage, expected_percentage)
+        self.assertEqual(score, expected_score)
+
+    def test_high_positive_score(self):
+        zones = np.array([20, 21, 29, 30, 0])
+        count = 100
+        expected_percentage = np.array([20.0, 21.0, 29.0, 30.0, 0.0])
+        expected_score = "High Positive"  # Based the weights and highest score
+
+        percentage, score = calculate_percentage_and_score(zones, count)
+
+        np.testing.assert_array_almost_equal(percentage, expected_percentage)
+        self.assertEqual(score, expected_score)
+
+    def test_positive_score(self):
+        zones = np.array([20, 27, 29, 24, 0])
+        count = 100
+        expected_percentage = np.array([20.0, 27.0, 29.0, 24.0, 0.0])
+        expected_score = "Positive"  # Based on the weights and the highest score
+
+        percentage, score = calculate_percentage_and_score(zones, count)
+
+        np.testing.assert_array_almost_equal(percentage, expected_percentage)
+        self.assertEqual(score, expected_score)
+
+    def test_low_positive_score(self):
+        zones = np.array([10, 20, 50, 10, 10])
+        count = 100
+        expected_percentage = np.array([10.0, 20.0, 50.0, 10.0, 10.0])
+        expected_score = "Low Positive"  # Based on the weights and the highest score
+
+        percentage, score = calculate_percentage_and_score(zones, count)
+
+        np.testing.assert_array_almost_equal(percentage, expected_percentage)
+        self.assertEqual(score, expected_score)
+
+    def test_negative_score(self):
+        zones = np.array([10, 19.5, 10, 60.5, 0])
+        count = 100
+        expected_percentage = np.array([10.0, 19.5, 10.0, 60.5, 0.0])
+        expected_score = "Negative"  # Based on the weights and the highest score
+
+        percentage, score = calculate_percentage_and_score(zones, count)
+
+        np.testing.assert_array_almost_equal(percentage, expected_percentage)
+        self.assertEqual(score, expected_score)
+
+    def test_background_score(self):
+        zones = np.array([3, 10, 10, 10, 67])
+        count = 100
+        expected_percentage = np.array([3.0, 10.0, 10.0, 10.0, 67.0])
+        expected_score = "Background"  # Based on the weights and the highest score
+
+        percentage, score = calculate_percentage_and_score(zones, count)
+
+        np.testing.assert_array_almost_equal(percentage, expected_percentage)
+        self.assertEqual(score, expected_score)
+
+    def test_background_66_percent(self):
+        zones = np.array([10, 20, 10, 10, 66])
+        count = 100
+        expected_percentage = np.array([10.0, 20.0, 10.0, 10.0, 66.0])
+        expected_score = "Positive"  # Background is exactly 66%, so the score is calculated
+
+        percentage, score = calculate_percentage_and_score(zones, count)
+
+        np.testing.assert_array_almost_equal(percentage, expected_percentage)
+        self.assertEqual(score, expected_score)
 
 
 class TestTileProcessing(unittest.TestCase):